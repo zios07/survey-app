3db9dca91fe5e82a1012ada54d9004a50bc3ed08
Zakaria Kaoukab <Zkaoukab@omnidata.local>
2018-07-03 11:59:05 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import * as Survey from 'survey-angular';
+import { surveyJSON } from './survey';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  
+  ngOnInit() {
+    var survey = new Survey.Model(surveyJSON);
+    survey.onComplete.add(this.sendDataToServer);
+    Survey.SurveyNG.render("surveyElement", {model:survey});
+  }
+
+  sendDataToServer(event) {
+    console.log(event.data);
+  }
+  
 }
---
+++ src/app/survey.ts
@@ -0,0 +1,61 @@
+export const surveyJSON = {
+  title: "Demo Survey",
+  showProgressBar: "top",
+  pages: [
+    {
+      name: "page1",
+      elements: [
+      {
+        type: "text",
+        name: "question1",
+        title: "Short answer",
+        isRequired: true
+      }
+      ]
+    },
+    {
+      name: "page2",
+      elements: [
+        {
+          type: "comment",
+          name: "question2",
+          title: "Paragraph",
+          isRequired: true
+         }
+      ]
+    },
+    {
+      name: "page3",
+      elements: [
+        {
+          type: "radiogroup",
+          name: "frameworkUsing",
+          title: "Do you use any front-end framework like Bootstrap?",
+          isRequired: true,
+          choices: [
+          "Yes",
+          "No"
+          ]
+        }
+      ]
+    },
+    {
+      name: "page4",
+      elements: [
+        {
+          type: "checkbox",
+          name: "framework",
+          title: "What front-end framework do you use?",
+          isRequired: true,
+          hasOther: true,
+          choices: [
+           "Bootstrap",
+           "Foundation"
+          ]
+         }
+      ]
+    }
+    ]
+   }
+  ]
+ };
